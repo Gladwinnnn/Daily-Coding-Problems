4d5772f8352f163279da860412557d8d7c0e3f7b
Gladwinnnn <gladwin1503@hotmail.com>
2020-10-02 23:59:29 +0800

    Update Q10.py

--- Q10.py
+++ Q10.py
@@ -4,14 +4,16 @@ def decode(dummy):
     placeHolder = []
     placeHolder2 = []
     for elements in dummy:
-        if int(elements) > 0 or int(elements) < 27:
+        if int(elements) > 0 and int(elements) < 27:
             placeHolder.append(elements)
-    if int(dummy[0:2]) > 0 or int(dummy[0:2]) < 27:
+    print(placeHolder)
+    if int(dummy[0:2]) > 0 and int(dummy[0:2]) < 27:
         placeHolder2.append(dummy[0:2])
         temp = dummy[2:]
         for elements in temp:
-            if int(elements) > 0 or int(elements) < 27:
+            if int(elements) > 0 and int(elements) < 27:
                 placeHolder2.append(elements)
     return len(placeHolder) + len(placeHolder2)
 
 print(decode(data))
+print(decode("0"))
