75fd2544243ce8d4b254d7d267818eac0d2978ac
Gladwinnnn <gladwin1503@hotmail.com>
2020-10-10 22:42:09 +0800

    Commented

--- Q21.py
+++ Q21.py
@@ -1,21 +1,21 @@
-Implement strStr().
+# Implement strStr().
 
-Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-Clarification:
+# Clarification:
 
-What should we return when needle is an empty string? This is a great question to ask during an interview.
+# What should we return when needle is an empty string? This is a great question to ask during an interview.
 
-For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+# For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 
-Input: haystack = "hello", needle = "ll"
-Output: 2
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
 
-Input: haystack = "aaaaa", needle = "bba"
-Output: -1
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
 
-Input: haystack = "", needle = ""
-Output: 0
+# Input: haystack = "", needle = ""
+# Output: 0
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
