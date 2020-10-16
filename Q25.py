08d3633339522b3b012ebf52ccae09c69500fa02
Gladwinnnn <gladwin1503@hotmail.com>
2020-10-16 19:23:16 +0800

    Daily Coding

---
+++ Q25.py
@@ -0,0 +1,22 @@
+# Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 3
+# Output: true
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 13
+# Output: false
+
+# Input: matrix = [], target = 0
+# Output: false
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0:
+            return False
+        placeHolder = []
+        for elements in matrix:
+            placeHolder += elements
+        return target in placeHolder
